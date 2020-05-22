a7b6f9944ce11aa0f1e63640550e822c4e83e954
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2020-05-22 12:50:16 +0200

    script: improve mvsec split script for davis and visensor frames.

--- mvsec_split_train_valid_test.py
+++ mvsec_split_train_valid_test.py
@@ -41,8 +41,15 @@ def convert_depth_to_image(i, data):
 def run(args):
     voxel_directory = os.path.join(args.input_directory, 'events/voxels') 
     depth_directory = os.path.join(args.input_directory, 'depth/data') 
+    frame_directory = os.path.join(args.input_directory, 'depth/frames') 
     input_voxels = sorted(glob.glob(os.path.join(voxel_directory, '*.npy')))
     input_depth = sorted(glob.glob(os.path.join(depth_directory, '*.npy')))
+    input_frame = sorted(glob.glob(os.path.join(frame_directory, '*.png')))
+
+    davis_directory = os.path.join(args.input_directory, 'rgb/davis_left_sync') 
+    visensor_directory = os.path.join(args.input_directory, 'rgb/visensor_left_sync') 
+    input_davis = sorted(glob.glob(os.path.join(davis_directory, '*.png')))
+    input_visensor = sorted(glob.glob(os.path.join(visensor_directory, '*.png')))
 
     ensure_dir(args.output_directory)
     voxel_train_directory = os.path.join(args.output_directory, "train/sequence_0000000000/events/voxels")
@@ -53,6 +60,18 @@ def run(args):
     depth_valid_directory = os.path.join(args.output_directory, "validation/sequence_0000000000/depth/data")
     depth_test_directory = os.path.join(args.output_directory, "test/sequence_0000000000/depth/data")
  
+    frame_train_directory = os.path.join(args.output_directory, "train/sequence_0000000000/depth/frames")
+    frame_valid_directory = os.path.join(args.output_directory, "validation/sequence_0000000000/depth/frames")
+    frame_test_directory = os.path.join(args.output_directory, "test/sequence_0000000000/depth/frames")
+ 
+    davis_train_directory = os.path.join(args.output_directory, "train/sequence_0000000000/rgb/davis")
+    davis_valid_directory = os.path.join(args.output_directory, "validation/sequence_0000000000/rgb/davis")
+    davis_test_directory = os.path.join(args.output_directory, "test/sequence_0000000000/rgb/davis")
+
+    visensor_train_directory = os.path.join(args.output_directory, "train/sequence_0000000000/rgb/visensor")
+    visensor_valid_directory = os.path.join(args.output_directory, "validation/sequence_0000000000/rgb/visensor")
+    visensor_test_directory = os.path.join(args.output_directory, "test/sequence_0000000000/rgb/visensor")
+ 
     ensure_dir(voxel_train_directory)
     ensure_dir(voxel_valid_directory)
     ensure_dir(voxel_test_directory)
@@ -61,6 +80,19 @@ def run(args):
     ensure_dir(depth_valid_directory)
     ensure_dir(depth_test_directory)
 
+    ensure_dir(frame_train_directory)
+    ensure_dir(frame_valid_directory)
+    ensure_dir(frame_test_directory)
+
+    ensure_dir(davis_train_directory)
+    ensure_dir(davis_valid_directory)
+    ensure_dir(davis_test_directory)
+
+    ensure_dir(visensor_train_directory)
+    ensure_dir(visensor_valid_directory)
+    ensure_dir(visensor_test_directory)
+
+    # Load the idx
     voxel_timestamps = genfromtxt(os.path.join(voxel_directory, 'timestamps.txt'), delimiter=' ')
     voxel_dic = dict(zip(voxel_timestamps[:,0], np.sort(voxel_timestamps[:,1])))
     voxel_idx = voxel_timestamps[:,0]
@@ -69,6 +101,7 @@ def run(args):
     voxel_boundary = genfromtxt(os.path.join(voxel_directory, 'boundary_timestamps.txt'), delimiter=' ')
     boundary_dic = dict(zip(voxel_boundary[:,0], zip(voxel_boundary[:,1], voxel_boundary[:,2])))
 
+    # Perform the split
     train_samples = int(0.7 * len(voxel_idx))
     if args.random:
         train_idx = np.sort(np.random.choice(np.arange(len(voxel_idx)), size=train_samples, replace=False))
@@ -87,80 +120,232 @@ def run(args):
     assert len(depth_idx) == len(input_depth)
 
     # Copy train dataset to folder
-    f=open("%s/timestamps.txt"%voxel_train_directory, "a+")
-    d=open("%s/timestamps.txt"%depth_train_directory, "a+")
+    f=open("%s/timestamps.txt"%frame_train_directory, "a+")
+    x=open("%s/timestamps.txt"%voxel_train_directory, "a+")
     b=open("%s/boundary_timestamps.txt"%voxel_train_directory, "a+")
-    for i, idx in enumerate(train_idx):
+    d=open("%s/timestamps.txt"%depth_train_directory, "a+")
+    a=open("%s/timestamps.txt"%davis_train_directory, "a+")
+    v=open("%s/timestamps.txt"%visensor_train_directory, "a+")
+    print("Generating train dataset...", len(train_idx))
+    for i, idx in tqdm.tqdm(enumerate(train_idx)):
+        # frame
+        frame_name = '%s/frame_%010d.png' % (frame_directory, idx)
+        new_frame_name = '%s/frame_%010d.png' % (frame_train_directory, i)
+        try:
+            os.system("cp " + frame_name + " " + new_frame_name)
+            f.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(frame_name))
+
+        # voxel
         voxel_name = '%s/event_tensor_%010d.npy' % (voxel_directory, idx)
         new_voxel_name = '%s/event_tensor_%010d.npy' % (voxel_train_directory, i)
-        os.system("cp " + voxel_name + " " + new_voxel_name)
-        f.write("%d %f\n" % (i, voxel_dic[idx]))
-        b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        try:
+            os.system("cp " + voxel_name + " " + new_voxel_name)
+            x.write("%d %f\n" % (i, voxel_dic[idx]))
+            b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        except IOError:
+            print ("File %s not accesible"%(voxel_name))
+
+
+        #depth
         depth_name = '%s/depth_%010d.npy' % (depth_directory, idx+1)
-        d.write("%d %f\n" % (i, depth_dic[idx+1]))
         if args.convert_to_frames:
-            data = np.load(depth_name)
-            img = convert_depth_to_image(i, data)
-            new_depth_name = '%s/frame_%010d.png' % (depth_train_directory, i)
-            cv2.imwrite(new_depth_name, img)
+            try:
+                data = np.load(depth_name)
+                img = convert_depth_to_image(i, data)
+                new_depth_name = '%s/frame_%010d.png' % (depth_train_directory, i)
+                cv2.imwrite(new_depth_name, img)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
         else:
             new_depth_name = '%s/depth_%010d.npy' % (depth_train_directory, i)
-            os.system("cp " + depth_name + " " + new_depth_name)
+            try:
+                os.system("cp " + depth_name + " " + new_depth_name)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
+
+
+        #Davis
+        davis_name = '%s/frame_%010d.png' % (davis_directory, idx) 
+        new_davis_name = '%s/frame_%010d.png' % (davis_train_directory, i)
+        try:
+            os.system("cp " + davis_name + " " + new_davis_name)
+            a.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(davis_name))
+
+
+        #VI-Sensor
+        visensor_name = '%s/frame_%010d.png' % (visensor_directory, idx) 
+        new_visensor_name = '%s/frame_%010d.png' % (visensor_train_directory, i)
+        try:
+            os.system("cp " + visensor_name + " " + new_visensor_name)
+            v.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(visensor_name))
+
 
 
     f.close()
+    x.close()
     b.close()
     d.close()
+    a.close()
+    v.close()
 
-    # Copy validaton dataset to folder
-    f=open("%s/timestamps.txt"%voxel_valid_directory, "a+")
-    d=open("%s/timestamps.txt"%depth_valid_directory, "a+")
+
+    # Copy validation dataset to folder
+    f=open("%s/timestamps.txt"%frame_valid_directory, "a+")
+    x=open("%s/timestamps.txt"%voxel_valid_directory, "a+")
     b=open("%s/boundary_timestamps.txt"%voxel_valid_directory, "a+")
-    for i, idx in enumerate(valid_idx):
+    d=open("%s/timestamps.txt"%depth_valid_directory, "a+")
+    a=open("%s/timestamps.txt"%davis_valid_directory, "a+")
+    v=open("%s/timestamps.txt"%visensor_valid_directory, "a+")
+    print("Generating validation dataset...", len(valid_idx))
+    for i, idx in tqdm.tqdm(enumerate(valid_idx)):
+        # frame
+        frame_name = '%s/frame_%010d.png' % (frame_directory, idx)
+        new_frame_name = '%s/frame_%010d.png' % (frame_valid_directory, i)
+        try:
+            os.system("cp " + frame_name + " " + new_frame_name)
+            f.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(frame_name))
+
+        # voxel
         voxel_name = '%s/event_tensor_%010d.npy' % (voxel_directory, idx)
         new_voxel_name = '%s/event_tensor_%010d.npy' % (voxel_valid_directory, i)
-        os.system("cp " + voxel_name + " " + new_voxel_name)
-        f.write("%d %f\n" % (i, voxel_dic[idx]))
-        b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        try:
+            os.system("cp " + voxel_name + " " + new_voxel_name)
+            x.write("%d %f\n" % (i, voxel_dic[idx]))
+            b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        except IOError:
+            print ("File %s not accesible"%(voxel_name))
+
+
+        #depth
         depth_name = '%s/depth_%010d.npy' % (depth_directory, idx+1)
-        d.write("%d %f\n" % (i, depth_dic[idx+1]))
         if args.convert_to_frames:
-            data = np.load(depth_name)
-            img = convert_depth_to_image(i, data)
-            new_depth_name = '%s/frame_%010d.png' % (depth_valid_directory, i)
-            cv2.imwrite(new_depth_name, img)
+            try:
+                data = np.load(depth_name)
+                img = convert_depth_to_image(i, data)
+                new_depth_name = '%s/frame_%010d.png' % (depth_valid_directory, i)
+                cv2.imwrite(new_depth_name, img)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
         else:
             new_depth_name = '%s/depth_%010d.npy' % (depth_valid_directory, i)
-            os.system("cp " + depth_name + " " + new_depth_name)
+            try:
+                os.system("cp " + depth_name + " " + new_depth_name)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
+
+
+        #Davis
+        davis_name = '%s/frame_%010d.png' % (davis_directory, idx) 
+        new_davis_name = '%s/frame_%010d.png' % (davis_valid_directory, i)
+        try:
+            os.system("cp " + davis_name + " " + new_davis_name)
+            a.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(davis_name))
+
+
+        #VI-Sensor
+        visensor_name = '%s/frame_%010d.png' % (visensor_directory, idx) 
+        new_visensor_name = '%s/frame_%010d.png' % (visensor_valid_directory, i)
+        try:
+            os.system("cp " + visensor_name + " " + new_visensor_name)
+            v.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(visensor_name))
 
     f.close()
+    x.close()
     b.close()
     d.close()
+    a.close()
+    v.close()
 
-    # Copy validaton dataset to folder
-    f=open("%s/timestamps.txt"%voxel_test_directory, "a+")
-    d=open("%s/timestamps.txt"%depth_test_directory, "a+")
+    # Copy test dataset to folder
+    f=open("%s/timestamps.txt"%frame_test_directory, "a+")
+    x=open("%s/timestamps.txt"%voxel_test_directory, "a+")
     b=open("%s/boundary_timestamps.txt"%voxel_test_directory, "a+")
-    for i, idx in enumerate(test_idx):
+    d=open("%s/timestamps.txt"%depth_test_directory, "a+")
+    a=open("%s/timestamps.txt"%davis_test_directory, "a+")
+    v=open("%s/timestamps.txt"%visensor_test_directory, "a+")
+    print("Generating test dataset...", len(test_idx))
+    for i, idx in tqdm.tqdm(enumerate(test_idx)):
+        # frame
+        frame_name = '%s/frame_%010d.png' % (frame_directory, idx)
+        new_frame_name = '%s/frame_%010d.png' % (frame_test_directory, i)
+        try:
+            os.system("cp " + frame_name + " " + new_frame_name)
+            f.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(frame_name))
+
+        # voxel
         voxel_name = '%s/event_tensor_%010d.npy' % (voxel_directory, idx)
         new_voxel_name = '%s/event_tensor_%010d.npy' % (voxel_test_directory, i)
-        os.system("cp " + voxel_name + " " + new_voxel_name)
-        f.write("%d %f\n" % (i, voxel_dic[idx]))
-        b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        try:
+            os.system("cp " + voxel_name + " " + new_voxel_name)
+            x.write("%d %f\n" % (i, voxel_dic[idx]))
+            b.write("%d %f %f\n" % (i, boundary_dic[idx][0], boundary_dic[idx][1]))
+        except IOError:
+            print ("File %s not accesible"%(voxel_name))
+
+
+        #depth
         depth_name = '%s/depth_%010d.npy' % (depth_directory, idx+1)
-        d.write("%d %f\n" % (i, depth_dic[idx+1]))
         if args.convert_to_frames:
-            data = np.load(depth_name)
-            img = convert_depth_to_image(i, data)
-            new_depth_name = '%s/frame_%010d.png' % (depth_test_directory, i)
-            cv2.imwrite(new_depth_name, img)
+            try:
+                data = np.load(depth_name)
+                img = convert_depth_to_image(i, data)
+                new_depth_name = '%s/frame_%010d.png' % (depth_test_directory, i)
+                cv2.imwrite(new_depth_name, img)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
         else:
             new_depth_name = '%s/depth_%010d.npy' % (depth_test_directory, i)
-            os.system("cp " + depth_name + " " + new_depth_name)
+            try:
+                os.system("cp " + depth_name + " " + new_depth_name)
+                d.write("%d %f\n" % (i, depth_dic[idx+1]))
+            except IOError:
+                print ("File %s not accesible"%(depth_name))
+
+
+        #Davis
+        davis_name = '%s/frame_%010d.png' % (davis_directory, idx) 
+        new_davis_name = '%s/frame_%010d.png' % (davis_test_directory, i)
+        try:
+            os.system("cp " + davis_name + " " + new_davis_name)
+            a.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(davis_name))
+
+
+        #VI-Sensor
+        visensor_name = '%s/frame_%010d.png' % (visensor_directory, idx) 
+        new_visensor_name = '%s/frame_%010d.png' % (visensor_test_directory, i)
+        try:
+            os.system("cp " + visensor_name + " " + new_visensor_name)
+            v.write("%d %f\n" % (i, voxel_dic[idx]))
+        except IOError:
+            print ("File %s not accesible"%(visensor_name))
 
     f.close()
+    x.close()
     b.close()
     d.close()
+    a.close()
+    v.close()
 
     print ('done')
 
